5746764e244ae915753b69a363716569343f86e7
devkassio <kassioxis@icloud.com>
2025-12-14 15:07:23 -0300

    fix: adicionar basename ao router para GitHub Pages

--- src/routes/indes.tsx
+++ src/routes/indes.tsx
@@ -5,29 +5,34 @@ import Focus from '../pages/focus';
 import HabitsPage from '../pages/habits';
 import LoginPage from '../pages/login';
 
-export const router = createBrowserRouter([
+export const router = createBrowserRouter(
+	[
+		{
+			path: '/',
+			element: (
+				<ProtectedRoute>
+					<HabitsPage />
+				</ProtectedRoute>
+			),
+		},
+		{
+			path: '/focus',
+			element: (
+				<ProtectedRoute>
+					<Focus />
+				</ProtectedRoute>
+			),
+		},
+		{
+			path: '/login',
+			element: <LoginPage />,
+		},
+		{
+			path: '/authentication',
+			element: <AuthPage />,
+		},
+	],
 	{
-		path: '/',
-		element: (
-			<ProtectedRoute>
-				<HabitsPage />
-			</ProtectedRoute>
-		),
-	},
-	{
-		path: '/focus',
-		element: (
-			<ProtectedRoute>
-				<Focus />
-			</ProtectedRoute>
-		),
-	},
-	{
-		path: '/login',
-		element: <LoginPage />,
-	},
-	{
-		path: '/authentication',
-		element: <AuthPage />,
-	},
-]);
+		basename: '/Elite-Tracker-Front',
+	}
+);
